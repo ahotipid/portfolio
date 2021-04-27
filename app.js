329f79cdeb794bae1ce58b03f5e1eed941e5366e
Kornthip <sang.kornthip@gmail.com>
2021-04-27 16:19:30 -0400

    added asset and update porfolio section:sparkles:

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ const app = {}
 app.burgerMenu = document.querySelector('nav.burgerMenu ul');
 app.burgerMenuLists = document.querySelectorAll('nav.burgerMenu li');
 app.burgerIcon = document.querySelector('nav.burgerMenu .burgerIcon');
+app.form = document.querySelector('form');
 
 //add event listenter to burger icon
-app.burgerAction = function () {
+app.burgerAction = () => {
     //make burger icon to trigger tranform on burger icon and ul height
-    app.burgerIcon.addEventListener('click', function() {
+    app.burgerIcon.addEventListener('click', () => {
         //after click on the icon, it toggle ul height
         app.burgerMenu.classList.toggle('addHeight');
         //toggle shape of burgerIcon 
@@ -18,23 +19,35 @@ app.burgerAction = function () {
 }
 
 //add event lister to burger menu list
-app.listAction = function () {
+app.listAction = () =>  {
     //make each list to trigger transform on burger icon and ul height
-    app.burgerMenuLists.forEach( function(list) {
-        list.addEventListener('click', function () {
+    app.burgerMenuLists.forEach( (list) => {
+        list.addEventListener('click', () => {
             //after click on each list, it toggle ul height
             app.burgerMenu.classList.toggle('addHeight');
             //toggle shape of burgerIcon 
             app.burgerIcon.classList.toggle('animateBurgerIcon');
-        })
+        });
     });
 }
 
-//create init
-app.init = function () {
+//empty form on Submit
+app.formEmpty = () => {
+    app.form.addEventListener('submit', () => {
+        const inputs = document.querySelectorAll('form input');
+        const textarea = document.querySelector('form textarea');
+        inputs.forEach( (input) => {
+            input.value = "";
+        });
+        textarea.value = "";
+    });
+}
+
+//create init and call all function
+app.init = () => {
     app.burgerAction();
     app.listAction();
-    console.log(app.burgerMenuLists);
+    app.formEmpty();
 }
 
 //initial all functions in the app 
